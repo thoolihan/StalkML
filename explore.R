599382bdb6d486469175a3dcab7c1362a66802e3
Tim Hoolihan <tim@hoolihan.net>
2016-03-25 07:59:41 -0400

    working on logistic regression

--- explore.R
+++ explore.R
@@ -1,6 +1,16 @@
+library(ggplot2)
+
 # load data
-market <- read.csv("stalk.csv", header = TRUE)
+market <- read.csv("data/stalk.csv", header = TRUE)
 
 # clean up
 market <- na.omit(market)
 market$IsDescending <- ifelse(market$IsDescending == 'Y', 1, 0)
+
+# average loss of a Thursday sale vs Tuesday sale for descending
+pos <- market[market$IsDescending == 1,]
+avg_loss <- mean(pos$TuePM - pos$ThuPM)
+print(c("tu - th loss", avg_loss))
+
+# plot descending by starting price
+qplot(market$IsDescending, market$SunAM)
---
+++ regression.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+
+# load data
+market <- read.csv("data/stalk.csv", header = TRUE)
+
+keep <- c("MonAM", "MonPM", "TueAM", "TuePM", "IsDescending")
+market <- market[, keep]
+
+# clean up
+market$IsDescending <- ifelse(market$IsDescending == 'Y', 1, 0)
+na.omit(market)
+
+# helpful variables
+n <- nrow(market)
+split <- floor(n * 0.7)
+itrain <- 1:split
+itest <- (split + 1):n
+
+# randomize
+market <- market[sample(n),]
+
+train <- market[itrain,]
+test <- market[itest,]
