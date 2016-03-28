7f720ff2fcc3a4964bd620fa69e78f4cd06e69aa
Tim Hoolihan <tim@hoolihan.net>
2016-03-28 15:45:20 -0400

    Simple cleanup

--- regression.R
+++ regression.R
@@ -4,10 +4,11 @@ library(ggplot2)
 
 # load data
 market <- read.csv("data/stalk.csv", header = TRUE)
-factors <- c("MonAM", "MonPM", "TueAM", "TuePM", "ch1", "ch2", "ch3")
+factors <- c("SunAM", "MonAM", "MonPM", "TueAM", "TuePM", "ch0", "ch1", "ch2", "ch3")
 keep <- c(factors, "IsDescending")
 
 # alter columns
+market$ch0 <- market$MonAM - market$SunAM
 market$ch1 <- market$MonPM - market$MonAM
 market$ch2 <- market$TueAM - market$MonPM
 market$ch3 <- market$TuePM - market$TueAM
@@ -38,11 +39,13 @@ train <- market[itrain,]
 test <- market[itest,]
 
 # create model
-model <- glm(IsDescending ~.,family=binomial(link='logit'),data=train)
+model <- glm(IsDescending ~ ., 
+             family=binomial(link='logit'), 
+             data = train)
 
 # predict
-test$prediction <- predict(model,newdata=test[,factors],type='response')
-test$prediction <- ifelse(test$prediction > 0.5,1,0)
+test$prediction <- predict(model, newdata = test[, factors], type='response')
+test$prediction <- ifelse(test$prediction > 0.5, 1, 0)
 
 # setup rsults
 test$tp <- ifelse(test$prediction == 1 & test$IsDescending == 1, 1, 0)
@@ -52,15 +55,17 @@ test$fn <- ifelse(test$prediction == 0 & test$IsDescending == 1, 1, 0)
 
 # accuracy
 acc <- (sum(test$tp) + sum(test$tn)) / nrow(test)
-print(paste('Accuracy: ',acc))
+print(paste('Accuracy: ', acc))
 
 # precision (tp / tp + fp)
 precision <- sum(test$tp) / (sum(test$tp) + sum(test$fp))
-print(paste('Precision',precision))
+if(is.na(precision) | precision == 0) { precision <- 0.001 }
+print(paste('Precision: ', precision))
 
 # recall (tp / tp + fn)
 recall <- sum(test$tp) / (sum(test$tp) + sum(test$fn))
-print(paste('Recall: ',recall))
+if(is.na(recall) | recall == 0) { recall <- 0.001 }
+print(paste('Recall: ', recall))
 
 f1 <- 2 * (precision * recall / (precision + recall))
 print(paste('F1 Score: ', f1))
