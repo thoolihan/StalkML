7fb7e4f3cf8e0fea2526131cbbf956821e700b22
Tim Hoolihan <tim@hoolihan.net>
2016-05-21 12:32:51 -0400

    simplified boost version with caret

--- boost.R
+++ boost.R
@@ -1,59 +1,40 @@
 library(xgboost)
+library(plyr)
 library(dplyr)
+library(caret)
 
 # load data
-market <- read.csv("data/stalk.csv", header = TRUE)
-factors <- c("bias", "SunAM", "MonAM", "MonPM", "TueAM", "TuePM", "ch0", "ch1", "ch2", "ch3")
-normalized_factors <- factors[!(factors %in% c("bias"))]
-keep <- c(factors, "IsDescending")
+market.classes <- c(rep('numeric', 14), 'character')
+market <- read.csv("data/stalk.csv", 
+                   colClasses = market.classes, 
+                   na.strings = c('', 'NA'),
+                   header = TRUE)
 
-# alter columns
-market <- mutate(market, 
-       bias = 1,
-       ch0 = MonAM - SunAM,
-       ch1 = MonPM - MonAM,
-       ch2 = TueAM - MonPM,
-       ch3 = TuePM - TueAM,
-       IsDescending = as.integer(ifelse(IsDescending == 'Y', 1, 0))
-       )
+market <- dplyr::select(market, -WedAM, -WedPM, -ThuAM, -ThuPM,
+                        -FriAM, -FriPM, -SatAM, -SatPM, -Week) %>%
+  mutate(SunAM = ifelse(is.na(SunAM), mean(SunAM, na.rm = TRUE), SunAM),
+         ch0 = MonAM - SunAM,
+         ch1 = MonPM - MonAM,
+         ch2 = TueAM - MonPM,
+         ch3 = TuePM - TueAM,
+         IsDescending = as.integer(revalue(IsDescending, replace = c('Y' = 1, 'N' = 0)))) %>%
+  na.omit() 
 
-# clean up
-market <- market[, keep]
-market <- na.omit(market)
-
-# helpful variables
-n <- nrow(market)
-split <- floor(n * 0.6)
-itrain <- 1:split
-
-# randomize
-market <- market[sample(n),]
-
-# split
-train <- market[itrain,]
-test <- market[-itrain,]
+train.rows <- createDataPartition(market$IsDescending, p = 0.6, list = FALSE)
+data.train <- market[train.rows,]
+data.test <- market[-(train.rows),]
 
 # create model
-X <- data.matrix(market[,factors])
-y <- market$IsDescending
+feature_matrix <- function(df) { dplyr::select(df, -IsDescending) %>% data.matrix()}
 
-model <- xgboost(data = X, label = y, 
-                    max.depth = 2, nthread = 4, nround = 2, 
-                    objective = "binary:logistic")
+model <- xgboost(data = feature_matrix(data.train), 
+                 label = data.train$IsDescending, 
+                 max.depth = 5, nthread = 4, nround = 10 , 
+                 objective = "binary:logistic")
 
 # predict
-test$predicted <- round(xgboost::predict(model, data.matrix(test)), 0)
+data.test$Prob <- xgboost::predict(model, feature_matrix(data.test))
+data.test$Output <- round(data.test$Prob, 0)
 
-tp <- sum(test$IsDescending == 1 & test$predicted == 1)
-tn <- sum(test$IsDescending == 0 & test$predicted == 0)
-fp <- sum(test$IsDescending == 0 & test$predicted == 1)
-fn <- sum(test$IsDescending == 1 & test$predicted == 0)
-
-results <- list(
-  acc = round((tp + tn) / nrow(test), 3),
-  prec = round(tp / (tp + fp), 3),
-  rec = round(tp / (fn + tp), 3)
-)
-results$f1 = round(2 * results$prec * results$rec / (results$prec + results$rec), 3)
-
+results <- confusionMatrix(data.test$Output, data.test$IsDescending)
 print(results)
