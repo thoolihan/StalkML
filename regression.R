c3d0c00fa1387a45df8a7c3fd842851a07025a9b
Tim Hoolihan <tim@hoolihan.net>
2016-05-24 10:31:14 -0400

    trying to do a glm regression

--- regression.R
+++ regression.R
@@ -1,73 +1,51 @@
 library(ggplot2)
+library(dplyr)
+library(foreach)
+library
+library(caret)
 
 # based on: http://www.r-bloggers.com/how-to-perform-a-logistic-regression-in-r/
 
 # load data
-market <- read.csv("data/stalk.csv", header = TRUE)
-factors <- c("SunAM", "MonAM", "MonPM", "TueAM", "TuePM", "ch0", "ch1", "ch2", "ch3")
-keep <- c(factors, "IsDescending")
+market <- read.csv("data/stalk.csv", 
+                   colClasses = c(rep('numeric', 14), 'factor'),
+                   header = TRUE)
 
 # alter columns
-market$ch0 <- market$MonAM - market$SunAM
-market$ch1 <- market$MonPM - market$MonAM
-market$ch2 <- market$TueAM - market$MonPM
-market$ch3 <- market$TuePM - market$TueAM
-market$IsDescending <- as.factor(ifelse(market$IsDescending == 'Y', 1, 0))
-
-# clean up
-market <- market[, keep]
-market <- na.omit(market)
+market <- mutate(market,
+                 ch0 = MonAM - SunAM,
+                 ch1 = MonPM - MonAM,
+                 ch2 = TueAM - MonPM,
+                 ch3 = TuePM - TueAM,
+                 even_start = SunAM %% 2) %>%
+  na.omit()
 
 # normalize
 normalize <- function(col) {
   return ((col - min(col)) / (max(col) - min(col)))
 }
 
-for(col in factors) { market[, col] <-  normalize(market[, col])}
-
-# helpful variables
-n <- nrow(market)
-split <- floor(n * 0.7)
-itrain <- 1:split
-itest <- (split + 1):n
-
-# randomize
-market <- market[sample(n),]
+for(col in c('SunAM', 'MonAM', 'MonPM', 'TueAM', 'TuePM', 
+             'ch0', 'ch1', 'ch2', 'ch3')) { 
+  market[, col] <-  normalize(market[, col])
+}
 
 # split
-train <- market[itrain,]
-test <- market[itest,]
+tri <- createDataPartition(market$IsDescending, p = 0.65, list = FALSE)
+train <- market[tri,]
+test <- market[-tri,]
 
 # create model
 
-model <- glm(IsDescending ~ ., 
+model <- glm(IsDescending ~ SunAM * MonAM * MonPM * TueAM * TuePM *
+               ch0 * ch1 * ch2 * even_start, 
              family=binomial(link='logit'), 
              data = train,
              control = list(maxit = 500))
 
 # predict
-test$prediction <- predict(model, test[, factors] )
-test$prediction <- ifelse(test$prediction > 0.5, 1, 0)
+test$prob <- predict(model, test)
+test$output <- factor(ifelse(test$prob > 0.5, 'Y', 'N'), 
+                      levels = levels(test$IsDescending))
+print(confusionMatrix(test$output, test$IsDescending))
 
-# setup rsults
-test$tp <- ifelse(test$prediction == 1 & test$IsDescending == 1, 1, 0)
-test$fp <- ifelse(test$prediction == 1 & test$IsDescending == 0, 1, 0)
-test$tn <- ifelse(test$prediction == 0 & test$IsDescending == 0, 1, 0)
-test$fn <- ifelse(test$prediction == 0 & test$IsDescending == 1, 1, 0)
-
-# accuracy
-acc <- (sum(test$tp) + sum(test$tn)) / nrow(test)
-print(paste('Accuracy: ', acc))
-
-# precision (tp / tp + fp)
-precision <- sum(test$tp) / (sum(test$tp) + sum(test$fp))
-if(is.na(precision) | precision == 0) { precision <- 0.001 }
-print(paste('Precision: ', precision))
-
-# recall (tp / tp + fn)
-recall <- sum(test$tp) / (sum(test$tp) + sum(test$fn))
-if(is.na(recall) | recall == 0) { recall <- 0.001 }
-print(paste('Recall: ', recall))
-
-f1 <- 2 * (precision * recall / (precision + recall))
-print(paste('F1 Score: ', f1))
